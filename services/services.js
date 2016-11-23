9e147b19120dc9a484bc46d6853ac3dd01d41800
shehantissera <shehan@duosoftware.com>
2016-11-23 22:55:18 +0530

    adding tokbox code

--- services/services.js
+++ services/services.js
@@ -401,15 +401,49 @@ angular.module('myApp.Services', []).
                         token = data.token;
 
                         var data = {
-                            apiKey : apiKey,
-                            sessionId : sessionId,
-                            token : token
+                            apiKey: apiKey,
+                            sessionId: sessionId,
+                            token: token
                         }
-                        if (onComplete) onComplete(data);
+                        initializeSession(apiKey,sessionId,data);
                     }).
                     error(function (data, status, headers, config) {
                         if (onError) onError(data);
                     });
+            }
+
+            function initializeSession(apiKey,sessionId,data) {
+                var session = OT.initSession(apiKey, sessionId);
+
+                // Subscribe to a newly created stream
+                session.on('streamCreated', function (event) {
+                    session.subscribe(event.stream, 'subscriber', {
+                        insertMode: 'append',
+                        width: '100%',
+                        height: '100%'
+                    });
+                });
+
+                session.on('sessionDisconnected', function (event) {
+                    console.log('You were disconnected from the session.', event.reason);
+                });
+
+                // Connect to the session
+                session.connect(token, function (error) {
+                    // If the connection is successful, initialize a publisher and publish to the session
+                    if (!error) {
+                        var publisher = OT.initPublisher('publisher', {
+                            insertMode: 'append',
+                            width: '100%',
+                            height: '100%'
+                        });
+
+                        session.publish(publisher);
+                    } else {
+                        console.log('There was an error connecting to the session: ', error.code, error.message);
+                    }
+                });
+                if (onComplete) onComplete(data);
             }
 
             return {
