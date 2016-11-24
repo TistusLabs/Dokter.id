ca62bcb265b6024cbcee7883576e789094f67f24
shehantissera <shehan@duosoftware.com>
2016-11-24 20:51:45 +0530

    WIP

--- services/services.js
+++ services/services.js
@@ -250,6 +250,7 @@ angular.module('myApp.Services', []).
                                     console.log("My ID: ", session.sessionId);
                                     // testing socket.io functionality
                                     //var socket = io.connect(AppURLs.socketServer, { secure: true, port: 4001 });
+                                    debugger;
                                     var socket = io.connect(AppURLs.socketServer);
                                     socket.emit('useronline', userObject.username);
 
