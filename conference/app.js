2bd73037df19d9c8eeef66e42745ee0e67af5c4b
shehantissera <shehan@duosoftware.com>
2016-11-28 01:34:27 +0530

    WOrking on the conference

---
+++ conference/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+angular.module('conferenceApp', [
+    'ngRoute',
+    'ngMaterial',
+    'ngMessages',
+    'myApp.Services'
+]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/call/:apikey/:sessionId/:token', {
+            templateUrl: 'index.html',
+            controller: 'mainController'
+        });
+    }])
+
+    .controller('mainController', ['$scope', '$rootScope', '$location', 'User', '$mdDialog','$routeParams','$route', function ($scope, $rootScope, $location, User, $mdDialog,$routeParams,$route) {
+
+        debugger;
+        $scope.apiKey = $routeParams.apikey;
+        $scope.sessionId = $routeParams.sessionId;
+        $scope.token = $routeParams.token;
+
+        $route.current;
+
+        $rootScope.ShowBusyContainer = function (message) {
+            $rootScope.isBusy = true;
+            $rootScope.bcmessage = message;
+        };
+
+        $rootScope.HideBusyContainer = function () {
+            $rootScope.isBusy = false;
+        };
+
+        $scope.initializeSession = function() {
+            var session = OT.initSession($scope.apiKey, $scope.sessionId);
+
+            // Subscribe to a newly created stream
+            session.on('streamCreated', function (event) {
+                session.subscribe(event.stream, 'subscriber', {
+                    insertMode: 'append',
+                    width: '100%',
+                    height: '100%'
+                });
+            });
+
+            session.on('sessionDisconnected', function (event) {
+                console.log('You were disconnected from the session.', event.reason);
+            });
+
+            // Connect to the session
+            session.connect($scope.token, function (error) {
+                // If the connection is successful, initialize a publisher and publish to the session
+                if (!error) {
+                    var publisher = OT.initPublisher('publisher', {
+                        insertMode: 'append',
+                        width: '100%',
+                        height: '100%'
+                    });
+
+                    session.publish(publisher);
+                } else {
+                    console.log('There was an error connecting to the session: ', error.code, error.message);
+                }
+            });
+        };
+        $scope.initializeSession();
+
+    }]);
