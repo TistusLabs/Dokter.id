237263c6e4c18393290ce8bc90f60491b7d68752
shehantissera <shehan@duosoftware.com>
2016-12-17 16:10:57 +0530

    date also fixed

--- profile/profile.js
+++ profile/profile.js
@@ -11,7 +11,7 @@ angular.module('myApp.profile', ['ngRoute'])
 
     .controller('profileControll', ['$scope', '$rootScope', '$location', 'User', function ($scope, $rootScope, $location, User) {
         $scope.days = [
-            "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"
         ];
 
         $scope.months = [
@@ -22,15 +22,25 @@ angular.module('myApp.profile', ['ngRoute'])
             "2010", "2009", "2008", "2007", "2006", "2005", "2004", "2003", "2002", "2001", "2000", "1999", "1998", "1997", "1996", "1995", "1994", "1993", "1992", "1991", "1990", "1989", "1988", "1987", "1986", "1985", "1984", "1983", "1982", "1981", "1980", "1979", "1978", "1977", "1976", "1975", "1974", "1973", "1972", "1971", "1970", "1969", "1968", "1967", "1966", "1965", "1964", "1963", "1962", "1961", "1960",
         ]
 
+        // setting the date of birth if the user already has data
+        if(!$rootScope.isNullOrEmptyOrUndefined($rootScope.userObject.dob)){
+            debugger
+            var d = new Date($rootScope.userObject.dob);
+            $scope.selectedDay = d.getDate().toString();
+            $scope.selectedMonth = $scope.months[d.getMonth()];
+            $scope.selectedYear = d.getFullYear().toString();
+        }
+
         $scope.updating = false;
         $scope.updateProfileDetails = function () {
 
-            debugger;
+            //debugger;
 
             // setting date of birth
 
             if (!$rootScope.isNullOrEmptyOrUndefined($scope.selectedMonth) && !$rootScope.isNullOrEmptyOrUndefined($scope.selectedDay) && !$rootScope.isNullOrEmptyOrUndefined($scope.selectedYear)) {
-                var d = new Date($scope.selectedMonth + " " + $scope.selectedDay + ", " + $scope.selectedYear);
+                //debugger
+                var d = new Date(parseInt($scope.selectedYear),$scope.months.indexOf($scope.selectedMonth), parseInt($scope.selectedDay));
                 var dateofBirth = d.toString();
                 $rootScope.userObject.dob = dateofBirth;
             }
