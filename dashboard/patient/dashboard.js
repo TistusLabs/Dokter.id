b1eea551da83f3981bf45e796b54aced6baad267
shehantissera <shehan@duosoftware.com>
2017-01-25 23:15:35 +0530

    WIP

--- dashboard/patient/dashboard.js
+++ dashboard/patient/dashboard.js
@@ -9,23 +9,42 @@ angular.module('myApp.patient.dashboard', ['ngRoute'])
         });
     }])
 
-    .controller('dashboardControll', ['$scope', '$rootScope', '$location', 'User','AppURLs', function ($scope, $rootScope, $location, User,AppURLs) {
+    .controller('dashboardControll', ['$scope', '$rootScope', '$location', 'User', 'AppURLs', function ($scope, $rootScope, $location, User, AppURLs) {
         /*$rootScope.checkSession();
         */
-        $scope.setUserOnline = function(username){
+        $scope.setUserOnline = function (username) {
             angular.forEach($scope.doctors, function (doctor, index) {
-                if(doctor.username == username){
-                    $scope.$apply(function(){
+                if (doctor.username == username) {
+                    $scope.$apply(function () {
                         doctor.status = "available";
                     });
                 }
             });
         }
+
+        $scope.setUserStatuses = function (data) {
+            angular.forEach(data, function (status, index) {
+                angular.forEach($scope.doctors, function (doctor, index) {
+                    if (doctor.username == status.username) {
+                        $scope.$apply(function () {
+                            doctor.status = status.status;
+                        });
+                    }
+                });
+            });
+        }
+
         var socket = io.connect(AppURLs.socketServer);
         socket.on('useronline', function (username) {
             //make a fd service call and update the user on DB
             debugger
             $scope.setUserOnline(username);
+        });
+
+        socket.on('statuschanged', function (data) {
+            //make a fd service call and update the user on DB
+            debugger
+            $scope.setUserStatuses(data);
         });
 
         $scope.isLoading = true;
@@ -36,23 +55,40 @@ angular.module('myApp.patient.dashboard', ['ngRoute'])
                 var passhash = CryptoJS.MD5(doc.username);
                 doc.profileimage = "http://www.gravatar.com/avatar/" + passhash;
             });
-            $scope.doctors = data;
-            $scope.isLoading = false;
+            var doctors = data;
+            $http.get(AppURLs.connectionStorage + '/status/getall').
+                success(function (data, status, headers, config) {
+
+                    angular.forEach(data, function (status, index) {
+                        angular.forEach(doctors, function (doctor, index) {
+                            if (doctor.username == status.username) {
+                                $scope.$apply(function () {
+                                    doctor.status = status.status;
+                                });
+                            }
+                        });
+                    });
+                    $scope.doctors = doctors;
+                    $scope.isLoading = false;
+                }).
+                error(function (data, status, headers, config) {
+                    console.log("Oops error");
+                });
         });
         client.onError(function (data) {
             alert(data.message);
             $scope.isLoading = false;
         });
         client.GetAllDoctors();
-        
-        $scope.scheduleCall = function(){
+
+        $scope.scheduleCall = function () {
             alert("Scheduleing call");
         }
 
         $scope.redirect = function (c) {
             $rootScope.setChatDataConnection(c);
             $scope.$apply(function () {
-                console.log("Redirecting page for '"+c.label +"' request...");
+                console.log("Redirecting page for '" + c.label + "' request...");
                 $rootScope.connectionStatus = "Connecting...";
                 $location.path("/chat/" + c.metadata + "/to");
             });
