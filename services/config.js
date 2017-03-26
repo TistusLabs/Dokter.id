f13aab2c657a7005c1a6f8b01b3633ee7190fcd4
shehantissera <shehan@duosoftware.com>
2017-03-26 15:56:28 +0530

    added the new heroku app

--- services/config.js
+++ services/config.js
@@ -1,2 +1,2 @@
 // do not inclde the final / on the URL. Make sure you enter the URL without the /
-var SERVER_BASE_URL = 'https://dokter-test.herokuapp.com';
+var SERVER_BASE_URL = 'https://doka-test.herokuapp.com';
